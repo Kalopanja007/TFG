dc9b500a20f8a0224d365a6759c9c17ac8eb0299
Kalopanja007 <kalopanja@gmail.com>
2023-06-13 13:36:06 +0200

    funciona flask en el docker-compose

--- dev_ops_tfg/envars.sh
+++ dev_ops_tfg/envars.sh
@@ -17,13 +17,32 @@ export M_SERVICE_2=son
 
 
 # ------------------------------------------------
-export FLASK_PORT=5000
-export FLASK_WORKDIR=flask_server
-export FLASK_SERVICE=flask_server
-export FLASK_IMG=flask_custom
-export FLASK_C_WORKDIR=/home/ubuntu/${FLASK_WORKDIR}
-export FLASK_EXEC_SCRIPT=base_n_flask.sh
+export FRONTEND_PORT=5000
+export FRONTEND_PORT=1000
+export FRONTEND_WORKDIR=text_descriptor
+export FRONTEND_SERVICE=text_descriptor
+export FRONTEND_IMG=frontend_flask
+export FRONTEND_C_WORKDIR=/home/ubuntu/${FRONTEND_WORKDIR}
+export FRONTEND_EXEC_SCRIPT=base_n_flask.sh
+export FRONTEND_DOCKERFILE=Dockerfile_text_descriptor
 
+# ------------------------------------------------
+
+export TEXT_DESCRIPTOR_PORT=1000
+export TEXT_DESCRIPTOR_WORKDIR=text_descriptor
+export TEXT_DESCRIPTOR_SERVICE=text_descriptor
+export TEXT_DESCRIPTOR_IMG=text_descriptor_flask
+export TEXT_DESCRIPTOR_C_WORKDIR=/home/ubuntu/${TEXT_DESCRIPTOR_WORKDIR}
+export TEXT_DESCRIPTOR_EXEC_SCRIPT=base_n_flask.sh
+export TEXT_DESCRIPTOR_DOCKERFILE=Dockerfile_text_descriptor
+# ------------------------------------------------
+
+export IMG_UPLOADER_PORT=5000
+export IMG_UPLOADER_WORKDIR=flask_server
+export IMG_UPLOADER_SERVICE=flask_server
+export IMG_UPLOADER_IMG=flask_custom
+export IMG_UPLOADER_C_WORKDIR=/home/ubuntu/${FRONTEND_WORKDIR}
+export IMG_UPLOADER_EXEC_SCRIPT=base_n_flask.sh
 # ------------------------------------------------
 # ------------------------------------------------
 export ASCIIART_SERVICE=ASCIIART_SERVICE
